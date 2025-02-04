25d56654b7f3c30d5c1728841427dd5e645fea74
ahmed rakan <ar.aldhafeeri11@gmail.com>
2025-02-04 13:32:23 +0300

    fix wrong edge

--- src/graph/graphs/crawler.ts
+++ src/graph/graphs/crawler.ts
@@ -20,13 +20,12 @@ const crawlWorkflow = new StateGraph(AppState)
   .addNode(AGENT_NAMES.hltasker, highLevelTasksNode)
   .addNode(AGENT_NAMES.crawler, crawlerNode)
   .addNode(AGENT_NAMES.reporter, reportNode)
-  .addNode(AGENT_NAMES.executor, executorNode)
   .addNode("call_tool", toolNode)
   // Begin with crawling the website
   .addEdge(START, AGENT_NAMES.hltasker)
   .addEdge(AGENT_NAMES.hltasker, AGENT_NAMES.crawler)
   // Let the executor decide if more crawling is required
-  .addConditionalEdges(AGENT_NAMES.executor, router, {
+  .addConditionalEdges(AGENT_NAMES.crawler, router, {
     continue: AGENT_NAMES.reporter,
     end: END,
     call_tool: "call_tool",
